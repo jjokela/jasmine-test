cbe0d46bff4201f286ab992a0cadf86660f22137
jjokela <jarmo.jokela@gmail.com>
2014-05-31 01:42:09 +0300

    Added spy tests

--- Tests/app/appSpec.js
+++ Tests/app/appSpec.js
@@ -8,4 +8,61 @@ describe('App', function () {
     xit('should be skipped', function () {
         expect(true).toBe(false);
     });
+
+    describe('App spy tests', function () {
+
+        it('should spy on callback', function () {
+            var spyCallback = jasmine.createSpy('mySpy'); // createSpy
+            CallbackTester(spyCallback);
+
+            expect(spyCallback).toHaveBeenCalled();
+        });
+
+        it('should spy on nothingDoer', function () {
+            var spy = spyOn(app, 'nothingDoer');
+            app.nothingDoer();
+
+            expect(spy).toHaveBeenCalled();
+        });
+
+        it('should spy on getQuantity', function () {
+            var spy = spyOn(app, 'getQuantity').and.returnValue(123);
+            var actual = app.getQuantity();
+
+            expect(actual).toEqual(123);
+        });
+
+        it('should spy on getQuantity fake', function () {
+            var spy = spyOn(app, 'getQuantity').and.callFake(function () {
+                console.log('returning 20 from fake');
+                return 20;
+            });
+            var actual = app.getQuantity();
+
+            expect(actual).toEqual(20);
+            expect(spy).toHaveBeenCalled();
+        });
+
+        it('should spy on getQuantity callthru', function () {
+            var spy = spyOn(app, 'getQuantity').and.callThrough();
+            var actual = app.getQuantity();
+
+            expect(actual).toEqual(10);
+            expect(spy).toHaveBeenCalled();
+        });
+
+        it('should spy on getQuantity throw', function () {
+            var spy = spyOn(app, 'getQuantity').and.throwError(new Error('problem!'));
+
+            var quantity;
+            try {
+                quantity = app.getQuantity();
+            } catch(ex) {
+                quantity = -999;
+            }
+
+            expect(quantity).toEqual(-999);
+            expect(spy).toHaveBeenCalled();
+        });
+    });
 });
--- app/app.js
+++ app/app.js
@@ -2,3 +2,17 @@
 function ExceptionThrower() {
     throw new Error('Catch!');
 }
+
+function CallbackTester(callback) {
+    callback();
+}
+
+var app = app || {};
+
+app.nothingDoer = function () {
+    
+}
+
+app.getQuantity = function () {
+    return 10;
+}
